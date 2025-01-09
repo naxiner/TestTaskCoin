4c3611dd0aa3356ca67f9408908e920552075ab5
naxiner <kirienkodenis2002@gmail.com>
2025-01-09 20:44:49 +0200

    Add CryptoCurrency Model

---
+++ TestTaskCoin/MVVM/Models/CryptoCurrency.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTaskCoin.MVVM.Models
+{
+    public class CryptoCurrency
+    {
+        public string Id { get; set; }
+        public int Rank { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Supply { get; set; }
+        public decimal? MaxSupply { get; set; }
+        public decimal MarketCapUsd { get; set; }
+        public decimal VolumeUsd24Hr { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal? ChangePercent24Hr { get; set; }
+        public decimal Vwap24Hr { get; set; }
+    }
+}
