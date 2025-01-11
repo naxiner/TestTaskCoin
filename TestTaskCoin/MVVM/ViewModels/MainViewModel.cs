82a18b8308dcd1d1018c8999ac8b07e65461f91c
naxiner <kirienkodenis2002@gmail.com>
2025-01-11 15:40:25 +0200

    Add Main Page and Main ViewModel

---
+++ TestTaskCoin/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using TestTaskCoin.Core;
+using TestTaskCoin.MVVM.Models;
+using TestTaskCoin.Services;
+
+namespace TestTaskCoin.MVVM.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private readonly ICoinCapService _coinCapService;
+        private ObservableCollection<CryptoCurrency> _cryptocurrencies;
+        public RelayCommand RefreshCommand { get; }
+
+        public ObservableCollection<CryptoCurrency> Cryptocurrencies
+        {
+            get => _cryptocurrencies;
+            set => SetProperty(ref _cryptocurrencies, value);
+        }
+
+        public MainViewModel(ICoinCapService coinCapService)
+        {
+            _coinCapService = coinCapService;
+            Cryptocurrencies = new ObservableCollection<CryptoCurrency>();
+            RefreshCommand = new RelayCommand(async () => await RefreshDataAsync());
+            _ = RefreshDataAsync();
+        }
+
+        private async Task RefreshDataAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var data = await _coinCapService.GetTopCryptoCurrenciesAsync();
+                Cryptocurrencies.Clear();
+                foreach (var crypto in data)
+                {
+                    Cryptocurrencies.Add(crypto);
+                }
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+                RefreshCommand.RaiseCanExecuteChanged();
+            }
+        }
+    }
+}
---
+++ TestTaskCoin/MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using TestTaskCoin.MVVM.ViewModels;
+using TestTaskCoin.Services;
+
+namespace TestTaskCoin.MVVM.Views
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(new CoinCapService());
+        }
+    }
+}
