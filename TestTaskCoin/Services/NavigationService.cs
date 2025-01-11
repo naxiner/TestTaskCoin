ad1cf9ea250c010a04f360140fec28d0fbdd7838
naxiner <kirienkodenis2002@gmail.com>
2025-01-11 17:14:05 +0200

    Add Navigation Service

---
+++ TestTaskCoin/Services/NavigationService.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace TestTaskCoin.Services
+{
+    public class NavigationService
+    {
+        private static Frame _mainFrame;
+
+        public static void Initialize(Frame frame)
+        {
+            _mainFrame = frame;
+        }
+
+        public static void NavigateToPage(Page page)
+        {
+            _mainFrame.Navigate(page);
+        }
+    }
+}
