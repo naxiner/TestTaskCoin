223e081e2dbff216d123c7fd9a9f04fbbc02824e
naxiner <kirienkodenis2002@gmail.com>
2025-01-11 15:30:50 +0200

    Add CoinCap Service

---
+++ TestTaskCoin/Services/CoinCapService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestTaskCoin.Constants;
+using TestTaskCoin.MVVM.Models;
+using Newtonsoft.Json;
+
+namespace TestTaskCoin.Services
+{
+    public class CoinCapService : ICoinCapService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CoinCapService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<CryptoCurrency>> GetTopCryptoCurrenciesAsync()
+        {
+            var url = $"{ApiConstants.BaseUrl}assets?limit=10";
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Failed to fetch data from API.");
+
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TopCryptoCurrenciesResponse>(json);
+
+            return result?.Data ?? new List<CryptoCurrency>();
+        }
+    }
+}
---
+++ TestTaskCoin/Services/ICoinCapService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestTaskCoin.MVVM.Models;
+
+namespace TestTaskCoin.Services
+{
+    public interface ICoinCapService
+    {
+        Task<List<CryptoCurrency>> GetTopCryptoCurrenciesAsync();
+    }
+}
