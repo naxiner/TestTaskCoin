0377fca6e9620ff816e5dac35f4f3e046dc8a40d
naxiner <kirienkodenis2002@gmail.com>
2025-01-14 17:19:05 +0200

    Add Market and MarketsResponse Models

---
+++ TestTaskCoin/MVVM/Models/Market.cs
@@ -0,0 +1,14 @@
+﻿namespace TestTaskCoin.MVVM.Models
+{
+    public class Market
+    {
+        public string ExchangeId { get; set; }
+        public string BaseId { get; set; }
+        public string QuoteId { get; set; }
+        public string BaseSymbol { get; set; }
+        public string QuoteSymbol { get; set; }
+        public decimal VolumeUsd24Hr { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal VolumePercent { get; set; }
+    }
+}
---
+++ TestTaskCoin/MVVM/Models/MarketsResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TestTaskCoin.MVVM.Models
+{
+    public class MarketsResponse
+    {
+        public List<Market> Data { get; set; }
+    }
+}
