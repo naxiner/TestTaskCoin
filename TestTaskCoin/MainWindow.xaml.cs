098b321d1835caf54a9dd4935a4d723402638340
naxiner <kirienkodenis2002@gmail.com>
2025-01-15 16:52:13 +0200

    Add Menu to Main Window

--- TestTaskCoin/MainWindow.xaml.cs
+++ TestTaskCoin/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 using TestTaskCoin.MVVM.Views;
 using TestTaskCoin.Services;
 
@@ -6,11 +8,52 @@ namespace TestTaskCoin
 {
     public partial class MainWindow : Window
     {
+        private bool isMenuOpen = false;
+
         public MainWindow()
         {
             InitializeComponent();
             NavigationService.Initialize(MainFrame);
             MainFrame.Navigate(new MainPage());
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            var storyboard = (Storyboard)FindResource(isMenuOpen ? "MenuClose" : "MenuOpen");
+            storyboard.Begin();
+            isMenuOpen = !isMenuOpen;
+        }
+
+        private void DarkOverlay_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isMenuOpen)
+            {
+                var storyboard = (Storyboard)FindResource("MenuClose");
+                storyboard.Begin();
+                isMenuOpen = false;
+            }
+        }
+
+        private void NavigateToMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new MainPage());
+            CloseMenu();
+        }
+
+        private void NavigateToSearch_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new SearchPage());
+            CloseMenu();
+        }
+
+        private void CloseMenu()
+        {
+            if (isMenuOpen)
+            {
+                var storyboard = (Storyboard)FindResource("MenuClose");
+                storyboard.Begin();
+                isMenuOpen = false;
+            }
         }
     }
 }
