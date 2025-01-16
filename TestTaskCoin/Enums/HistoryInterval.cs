0f52c2dd8117cccd8d01edaac64a9035b24db363
naxiner <kirienkodenis2002@gmail.com>
2025-01-16 14:45:08 +0200

    Add Enum HistoryInterval

---
+++ TestTaskCoin/Enums/HistoryInterval.cs
@@ -0,0 +1,15 @@
+﻿namespace TestTaskCoin.Enums
+{
+    public enum HistoryInterval
+    {
+        M1,   // 1 minute
+        M5,   // 5 minutes
+        M15,  // 15 minutes
+        M30,  // 30 minutes
+        H1,   // 1 hour
+        H2,   // 2 hours
+        H6,   // 6 hours
+        H12,  // 12 hours
+        D1    // 1 day
+    }
+}
